b4aa6b70e4ffad4433f97fa3aa12bc6ef5bc412d
BryanBlinDorard <brybrybbd@gmail.com>
2022-12-15 17:25:23 +0100

    un pseudo une personne

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -2,24 +2,49 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.List;
 import java.time.format.DateTimeFormatter; 
 import java.time.LocalDateTime; 
 
 public class ClientHandler implements Runnable{
     private Socket client;
-    private List<Socket> clients;
-    public ClientHandler(Socket socket, List<Socket> clients){
+    private List<HashMap<Socket, String>> clients;
+    public ClientHandler(Socket socket, List<HashMap<Socket, String>> clients){
         this.client = socket;
         this.clients = clients;
     }
     
     public void run(){
         try {
-            // On récupère le nom du client
+            // On récupère le nom du client et on vérifie si il est déjà utilisé
+            Boolean isNameSet = false;
+            String nomClient = "";
             DataInputStream in = new DataInputStream(client.getInputStream());
-            String nomClient = in.readUTF();
-            System.out.println(nomClient + " vient de se connecter.");
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            while (!isNameSet) {
+                nomClient = in.readUTF();
+                if (nomClient.length() > 0) {
+                    Boolean isNameUsed = false;
+                    for (HashMap<Socket, String> client : clients) {
+                        Socket keySocket = client.keySet().iterator().next();
+                        if (client.get(keySocket).equals(nomClient)) {
+                            isNameUsed = true;
+                            out.writeUTF("true");
+                        }
+                    }
+                    if (!isNameUsed) {
+                        for (HashMap<Socket, String> client : clients) {
+                            Socket keySocket = client.keySet().iterator().next();
+                            if (keySocket == this.client) {
+                                client.put(keySocket, nomClient);
+                            }
+                        }
+                        isNameSet = true;
+                        out.writeUTF("false");
+                    }
+                }
+            }
 
             while(true){
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -30,15 +55,23 @@ public class ClientHandler implements Runnable{
                 System.out.println(msg_a_envoyer);
 
                 // On envoie le message à tous les clients
-                for(Socket s : clients){
-                    if(s != client){
-                        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-                        out.writeUTF(msg_a_envoyer);
+                for (HashMap<Socket, String> client : clients) {
+                    Socket keySocket = client.keySet().iterator().next();
+                    if (keySocket != this.client) {
+                        DataOutputStream out2 = new DataOutputStream(keySocket.getOutputStream());
+                        out2.writeUTF(msg_a_envoyer);
                     }
                 }
             }
         } catch (IOException e) {
             System.out.println("Un client vient de se déconnecter.");
+            for (HashMap<Socket, String> client : clients) {
+                Socket keySocket = client.keySet().iterator().next();
+                if (keySocket == this.client) {
+                    clients.remove(client);
+                    break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Server{
@@ -9,13 +10,15 @@ public class Server{
         // On crée un serveur sur le port 1234
         ServerSocket serveur = new ServerSocket(1234);
         // On crée une liste qui va contenir les clients
-        List<Socket> clients = new ArrayList<Socket>();
+        List<HashMap<Socket, String>> clients = new ArrayList<HashMap<Socket, String>>();
         System.out.println("Serveur lancé, en attente de connexion...");
         while(true){
             // On attend une connexion d'un client
             Socket client = serveur.accept();
             System.out.println("Un client vient de se connecter.");
-            clients.add(client);
+            HashMap<Socket, String> clientMap = new HashMap<Socket, String>();
+            clientMap.put(client, "");
+            clients.add(clientMap);
 
             // On lance un thread qui va gérer la connexion avec ce client
             Thread t = new Thread(new ClientHandler(client, clients));
