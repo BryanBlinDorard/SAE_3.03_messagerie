f7b607c2301c8109a017c0df2a9b2bf7deb50b8c
BryanBlinDorard <brybrybbd@gmail.com>
2022-12-15 17:37:06 +0100

    exception si serveur fermé

--- src/Affichage.java
+++ src/Affichage.java
@@ -1,5 +1,6 @@
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.SocketException;
 
 public class Affichage implements Runnable {
     private DataInputStream in;
@@ -12,8 +13,11 @@ public class Affichage implements Runnable {
                 // On lit le message envoyé par le serveur
                 String message = in.readUTF();
                 System.out.println(message);
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                if (e instanceof SocketException) {
+                    System.out.println("Le serveur a fermé la connexion");
+                    System.exit(0);
+                }
             }
         }
     }
